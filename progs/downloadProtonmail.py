b59e40cd0aa45b2c6e9201d5c7c3cdbdcd20fbb6
Mathew Lewis <mathewlewis@Mathews-MacBook-Pro.local>
2020-05-21 00:26:22 -0700

    .

---
+++ progs/downloadProtonmail.py
@@ -0,0 +1,20 @@
+# Mathew Lewis, May 2020
+# This program downloads emails from protonmail
+# run the code, then switch tabs to protonmail open in firefox
+
+from pyautogui import locate,scroll,click,screenshot,press,moveTo
+from time import sleep
+import numpy as np; from PIL import Image; from scipy import signal
+
+def clickOn(imgFile,x,y,w,h):
+	B = None
+	small = Image.open(imgFile)
+	while B == None: B = locate(small,screenshot(region=(x,y,w,h)))
+	click(x/2+B.left/2+B.width/4,y/2+B.top/2+B.height/4)
+
+while True:
+	clickOn("buttons/more.png",1200*2,250*2,220*2,650*2)
+	scroll(-19); sleep(.2); moveTo(500,500)
+	clickOn("buttons/expo.png",1230*2,250*2, 30*2,650*2)
+	clickOn("buttons/ok.png",   800*2,560*2,100*2, 40*2)
+	press('escape'); press('down'); press('enter')
