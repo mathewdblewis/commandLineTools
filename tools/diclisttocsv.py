7b7d79b13af8fa54ee575343c668d57ff7977cd3
Mathew Lewis <mathewlewis@MacBook-Pro.local>
2020-09-27 01:33:32 -0700

    .

--- tools/diclisttocsv.py
+++ tools/diclisttocsv.py
@@ -1,5 +1,5 @@
 
-def diclistto2dAr(diclist):
+def diclisttoar(diclist):
 	cols = list(set([x for y in diclist for x in y]))
 	L = [[x for x in cols]]
 	for dic in diclist:
@@ -12,9 +12,7 @@ def diclistto2dAr(diclist):
 def artocsv(ar):
 	return '\n'.join([','.join([str(x) for x in row]) for row in ar])
 
-
-x = diclistto2dAr([{'asdf':42},{2:57},{3:21,'asdf':66}])
-
-print(artocsv(x))
+# x = diclisttoar([{'asdf':42},{2:57},{3:21,'asdf':66}])
+# print(artocsv(x))
 
 
