a2558974d0ef5fe2e22bf0fab12e69bd830df5a5
lewismathewdb <lewismathewdb@protonmail.com>
2020-10-25 13:40:46 -0700

    .

--- Notes/monomials.py
+++ Notes/monomials.py
@@ -1,5 +1,8 @@
 def monomials(deg,dim):
-	if dim == 1: return [[k] for k in range(deg+1)]
-	ans = [[m + [i] for i in range(deg-sum(m)+1)] for m in monomials(deg,dim-1)]
-	return [x for y in ans for x in y]
+	if dim == 0: return [[]]
+	ans = [[[i]+m for i in range(deg-sum(m)+1)[::-1]] for m in monomials(deg,dim-1)]
+	ans = [x for y in ans for x in y]
+	return [x[1:] for x in sorted([[sum(x)]+x for x in ans])][::-1]
+
+
 
