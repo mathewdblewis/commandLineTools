10de73ffd3821116aeeaf77008ed6df566f5e9cf
lewismathewdb <lewismathewdb@protonmail.com>
2020-11-17 15:41:54 -0800

    .

---
+++ Notes/email.py
@@ -0,0 +1,17 @@
+# go here to turn on less secure apps:
+# https://myaccount.google.com/lesssecureapps?pli=1
+
+import smtplib
+
+email = 'stevenrobinson48291@gmail.com' # Your email
+password = '8gg6SXNNZjkDXmg' # Your email account password
+send_to_email = 'mathewdblewis@gmail.com' # Who you are sending the message to
+message = 'This is my message' # The message in the email
+
+server = smtplib.SMTP('smtp.gmail.com', 587) # Connect to the server
+server.starttls() # Use TLS
+server.login(email, password) # Login to the email server
+server.sendmail(email, send_to_email , message) # Send the email
+server.quit() # Logout of the email server
+
+
